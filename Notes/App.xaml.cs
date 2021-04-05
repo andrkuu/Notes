f68d891aec585cb77a239627cb7f891eb83276ea
andre <andrku@tlu.ee>
2021-04-05 16:02:53 +0300

    Add project files.

---
+++ Notes/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using Notes.Data;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class App : Application
+    {
+        static NoteDatabase database;
+        static string ExternalPath;
+        
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+                    string filename = System.IO.Path.Combine(path, "memorytype.txt");
+
+                    using (var streamWriter = new StreamWriter(filename, true))
+                    {
+                        streamWriter.WriteLine("internal");
+                    }
+
+
+                    using (var streamReader = new StreamReader(filename))
+                    {
+                        string content = streamReader.ReadToEnd();
+                        System.Diagnostics.Debug.WriteLine(content);
+                        if (content.Equals("internal"))
+                        {
+                            database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                        }
+                        else
+                        {
+                            database = new NoteDatabase(Path.Combine(ExternalPath, "Notes.db3"));
+                        }
+                    }
+
+                }
+                return database;
+            }
+        }
+
+        public App(string ExternalPath)
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+            App.ExternalPath = ExternalPath;
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string filename = System.IO.Path.Combine(path, "memorytype.txt");
+            if (InternalButton.IsChecked)
+            {
+                using (var streamWriter = new StreamWriter(filename, true))
+                {
+                    streamWriter.WriteLine("internal");
+                }
+            }
+            else if (ExternalButton.IsChecked)
+            {
+                using (var streamWriter = new StreamWriter(filename, true))
+                {
+                    streamWriter.WriteLine("external");
+                }
+            }         
+        }
+
+        private async System.Threading.Tasks.Task RadioButton_CheckedChangedAsync(object sender, CheckedChangedEventArgs e)
+        {
+            
+        }
+    }
+}
